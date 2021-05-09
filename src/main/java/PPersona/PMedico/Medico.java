de2337c8fce255d5ba8007cbe260ca7c70b1643e
augustomtt <65096407+augustomtt@users.noreply.github.com>
2021-05-09 18:38:43 -0300

    Los getter/setter pueden estar en la clase Médico
    Los getter/setter pueden estar en la clase Médico, evitando repeticion de código y quedan mas claras las funciones de cada tipo de médico, que opinan de esto?

--- src/main/java/PPersona/PMedico/Medico.java
+++ src/main/java/PPersona/PMedico/Medico.java
@@ -5,63 +5,119 @@ import java.util.GregorianCalendar;
 import java.util.Iterator;
 import PPersona.PPacientes.Paciente;
 
-public abstract class Medico implements IMedico{
-    /*
-    *   Esta es la "Master Class" que controla a todos los medicos con sus respectivos decorados.
-    *
-    *   **La variable "matriculaMedicos" es una variable de clase, es decir
-    *       todas las instancias de la clase tienen el mismo valor independientemente de en que momento
-    *       fue creada y con esta se determina la matricula de cada instancia para que sea unicos
-    *
-    *   **La variable "sueldo básico" es el sueldo base asignado a cada medico, el cual es modificado
-    *       por cada decorado que se le haga a las clases hijas de medico
-    *
-    *
-    *
-    * */
-    public static int matriculaMedicos=0;
-    protected int matricula;
-    protected String nombre;
-    protected String apellido;
-    protected int DNI;
-    protected String domicilio;
-    protected String ciudad;
-    protected String telefono;
-    protected ArrayList<Consulta> consultas = new ArrayList<Consulta>();
+public abstract class Medico implements IMedico {
+	/*
+	 * Esta es la "Master Class" que controla a todos los medicos con sus
+	 * respectivos decorados.
+	 *
+	 * **La variable "matriculaMedicos" es una variable de clase, es decir todas las
+	 * instancias de la clase tienen el mismo valor independientemente de en que
+	 * momento fue creada y con esta se determina la matricula de cada instancia
+	 * para que sea unicos
+	 *
+	 * **La variable "sueldo básico" es el sueldo base asignado a cada medico, el
+	 * cual es modificado por cada decorado que se le haga a las clases hijas de
+	 * medico
+	 *
+	 *
+	 *
+	 */
+	public static int matriculaMedicos = 0;
+	protected int matricula;
+	protected String nombre;
+	protected String apellido;
+	protected int DNI;
+	protected String domicilio;
+	protected String ciudad;
+	protected String telefono;
+	protected ArrayList<Consulta> consultas = new ArrayList<Consulta>();
 
+	private String tipo = "medico";// esto se saca despues, solo es para pruebas
 
-
-    private String tipo="medico";//esto se saca despues, solo es para pruebas
-    @Override
-    public String saludo(){// esta funcion se quita, es para probar el patron
-        return this.tipo;
-    }
-    /*
-    * para setear desde el main si cambia el basico para que nadie toque esta clase UwU
-    *
-    * */
-
-    @Override
-    public float getHonorario(){
-        return sueldoBasico;
-    }
-
-    @Override
-    public int getMatricula() {
-        return this.matricula;
-    }
-
-    @Override
-    public void iniciaMatricula() {
-    }
-    
 	@Override
-    public void atenderPaciente(GregorianCalendar fecha,int historiaClinica) {
-			consultas.add( new Consulta(fecha,historiaClinica));
-    }
-    @Override
-    public ArrayList<Consulta> getConsultas() {
-        return this.consultas;
-    }
-		
+	public String saludo() {// esta funcion se quita, es para probar el patron
+		return this.tipo;
+	}
+	/*
+	 * para setear desde el main si cambia el basico para que nadie toque esta clase
+	 * UwU
+	 *
+	 */
+
+	@Override
+	public float getHonorario() {
+		return sueldoBasico;
+	}
+
+	@Override
+	public int getMatricula() {
+		return this.matricula;
+	}
+
+	public void iniciaMatricula() {
+		Medico.matriculaMedicos++;
+		this.matricula = Medico.matriculaMedicos;
+	}
+
+	@Override
+	public void atenderPaciente(GregorianCalendar fecha, int historiaClinica) {
+		consultas.add(new Consulta(fecha, historiaClinica));
+	}
+
+	@Override
+	public ArrayList<Consulta> getConsultas() {
+		return this.consultas;
+	}
+
+	@Override
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	@Override
+	public String getApellido() {
+		return this.apellido;
+	}
+
+	@Override
+	public void setDNI(int DNI) {
+		this.DNI = DNI;
+	}
+
+	@Override
+	public int getDNI() {
+		return this.DNI;
+	}
+
+	@Override
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+
+	}
+
+	@Override
+	public String getDomicilio() {
+		return this.domicilio;
+	}
+
+	@Override
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	@Override
+	public String getCiudad() {
+		return this.ciudad;
+	}
+
+	@Override
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	@Override
+	public String getTelefono() {
+		return this.telefono;
+	}
+
 }
